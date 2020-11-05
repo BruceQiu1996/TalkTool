9dea9a0ed3d6d2202a0eece0a5605d2ad82d0e11
BruceQiu1996 <18852951552@163.com>
2020-11-05 15:20:42 +0800

    init code

---
+++ ChatAPI/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ChatAPI.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        } 
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task Login()
+        {
+            
+        }
+    }
+}
---
+++ ClobtoticsTalk.Module.Core/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClobtoticsTalk.Module.Core.Controllers
+{
+    public class AccountController:ControllerBase
+    {
+
+    }
+}
